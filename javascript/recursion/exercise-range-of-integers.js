ab87584e927859e909ddefbc63229a4d44c02211
Safei Muslim <safei.muslim@bukalapak.com>
2025-11-26 15:56:12 +0700

    task range

---
+++ javascript/recursion/exercise-range-of-integers.js
@@ -0,0 +1,22 @@
+/*
+
+Challenge:
+Write a JavaScript program to get integers in the range (x, y) using recursion.
+
+Example : range(2, 9)
+
+Expected Output : [3, 4, 5, 6, 7, 8]
+
+*/
+
+
+function getRangeNumber(x,y) {
+ if(y - x === 1) {
+  return []
+ } else {
+  y -= 1
+  return [...getRangeNumber(x, y), y];
+ }
+}
+
+console.log(getRangeNumber(2, 9))
